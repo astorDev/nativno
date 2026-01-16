67064edaaa810bcc320f1fb4ad4e81f8a1dc4863
Egor Tarasov <vosarat1995@google.com>
2026-01-16 07:39:26 +0100

    Resize: First Playground (MAUI Blazor, MacOS)

---
+++ resize/maui-blazor-mac/play/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Nativno.Resize.MauiBlazorMac.Playground;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ resize/maui-blazor-mac/play/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Nativno.Resize.MauiBlazorMac.Playground;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
