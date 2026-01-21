9ce96a66550ddca1864a8780435a29e593cb916a
Egor Tarasov <vosarat1995+1@gmail.com>
2026-01-21 09:43:07 +0100

    bluetooth/maui-blazor-mac/play: Devices Scan Page (#3)
    * Bluetooth MAUI Blazor Mac Play: Initial Failing Version (due to a lack of permissions)
    
    * bluetooth/maui-blazor-mac: Fully Functional Scan Page
    
    ---------
    
    Co-authored-by: Egor Tarasov <vosarat1995@google.com>

---
+++ bluetooth/maui-blazor-mac/play/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Nativno.Bluetooth.Mac.Playground;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "Nativno.Bluetooth.Mac.Playground" };
+	}
+}
---
+++ bluetooth/maui-blazor-mac/play/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Nativno.Bluetooth.Mac.Playground;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
