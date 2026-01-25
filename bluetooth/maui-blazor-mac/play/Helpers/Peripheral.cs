d056f17ba62f39f039ac80d977190338490e0284
Egor Tarasov <vosarat1995@google.com>
2026-01-25 16:41:29 +0100

    bluetooth home page cleaned up

--- bluetooth/maui-blazor-mac/play/Helpers/Peripheral.cs
+++ bluetooth/maui-blazor-mac/play/Helpers/Peripheral.cs
@@ -20,6 +20,132 @@ public static class PeripheralHelper
 public class SelectedService
 {
     public CBService? Instance { get; set; }
-    
-    
+}
+
+public class TrackedPeripheral(CBDiscoveredPeripheralEventArgs discovery, DateTime discoveredAt)
+{
+    public CBDiscoveredPeripheralEventArgs Discovery { get; private set; } = discovery;
+    public DateTime DiscoveredAt { get; private set; } = discoveredAt;
+    public PeripheralConnectionState ConnectionState { get; set; } = PeripheralConnectionState.Disconnected;
+
+    public void RegisterNewDiscovery(CBDiscoveredPeripheralEventArgs discovery, DateTime discoveredAt)
+    {
+        Discovery = discovery;
+        DiscoveredAt = discoveredAt;
+    }
+}
+
+public class TrackedPeripheralCollection
+{
+    public Dictionary<string, TrackedPeripheral> Items { get; } = new ();
+    public Notifier<TrackedPeripheral> ConnectionStatusUpdatedNotifier { get; } = new ();
+    public Notifier ListUpdatedNotifier { get; } = new ();
+
+    public void SubscribeToPeripheralsDiscoveries(CBCentralManager manager)
+    {
+        manager.DiscoveredPeripheral += (_, e) =>
+        {
+            RegisterDiscoveryOnNamedPeripheral(e);
+        };
+    }
+
+    public void RegisterDiscoveryOnNamedPeripheral(CBDiscoveredPeripheralEventArgs e)
+    {
+        var id = e.Peripheral.Identifier.ToString();
+        var name = e.Peripheral.Name;
+
+        if (String.IsNullOrWhiteSpace(name))
+        {
+            return;
+        }
+
+        if (Items.TryGetValue(id, out var existing))
+        {
+            existing.RegisterNewDiscovery(e, DateTime.UtcNow);
+        }
+        else
+        {
+            Items[id] = new TrackedPeripheral(e, DateTime.UtcNow);
+        }
+
+        ListUpdatedNotifier.Notify();
+    }
+
+    public void RemoveOldDiscoveries(TimeSpan maxAge)
+    {
+        var threshold = DateTime.UtcNow - maxAge;
+        var toRemove = Items
+            .Where(kv => kv.Value.DiscoveredAt < threshold)
+            .Select(kv => kv.Key);
+
+        foreach (var id in toRemove)
+        {
+            Items.Remove(id);
+        }
+    }
+
+    public TrackedPeripheral Tracked(CBPeripheral peripheral)
+    {
+        var id = peripheral.Identifier.ToString();
+        return Items[id];
+    }
+
+    public void SubscribeToConnectionStatuses(CBCentralManager manager)
+    {
+        manager.ConnectedPeripheral += (_, e) => UpdateConnectionStatus(e.Peripheral, PeripheralConnectionState.Connected);
+        manager.DisconnectedPeripheral += (_, e) =>  UpdateConnectionStatus(e.Peripheral, PeripheralConnectionState.Disconnected);
+        manager.DidDisconnectPeripheral += (_, e) => UpdateConnectionStatus(e.Peripheral, PeripheralConnectionState.Disconnected);
+        manager.FailedToConnectPeripheral += (_, e) => UpdateConnectionStatus(e.Peripheral, PeripheralConnectionState.Disconnected);
+    }
+
+    public void UpdateConnectionStatus(CBPeripheral peripheral, PeripheralConnectionState state)
+    {
+        var tracked = Tracked(peripheral);
+        tracked.ConnectionState = state;
+
+        ConnectionStatusUpdatedNotifier.Notify(tracked);
+    }
+}
+
+public enum PeripheralConnectionState
+{
+    Disconnected,
+    Connecting,
+    Connected
+}
+
+public class Notifier<TEventArgs>
+{
+    public List<Action<TEventArgs>> Listeners { get; } = new ();
+
+    public void Subscribe(Action<TEventArgs> listener)
+    {
+        Listeners.Add(listener);
+    }
+
+    public void Notify(TEventArgs args)
+    {
+        foreach (var listener in Listeners)
+        {
+            listener(args);
+        }
+    }
+}
+
+public class Notifier
+{
+    public List<Action> Listeners { get; } = new ();
+
+    public void Subscribe(Action listener)
+    {
+        Listeners.Add(listener);
+    }
+
+    public void Notify()
+    {
+        foreach (var listener in Listeners)
+        {
+            listener();
+        }
+    }
 }
--- bluetooth/maui-blazor-mac/play/MauiProgram.cs
+++ bluetooth/maui-blazor-mac/play/MauiProgram.cs
@@ -1,7 +1,10 @@
-﻿using CommunityToolkit.Maui;
+﻿using System.Reflection;
+using CommunityToolkit.Maui;
 using CoreBluetooth;
 using CoreFoundation;
+using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Logging;
+using Nativno.Bluetooth.Mac.Playground.Components.Pages;
 using Nativno.Bluetooth.Mac.Playground.Helpers;
 
 namespace Nativno.Bluetooth.Mac.Playground;
@@ -23,10 +26,15 @@ public static class MauiProgram
 		builder.Services.AddSingleton<PeripheralsCollection>();
 		builder.Services.AddSingleton<CBCentralManager>(_ => new (dispatchQueue: DispatchQueue.MainQueue));
 		builder.Services.AddSingleton<SelectedService>();
+		builder.Services.AddSingleton<TrackedPeripheralCollection>();
 
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
 		builder.Logging.AddDebug();
+#endif
+		
+#if DEBUG
+		return builder.BuildAndValidateBlazor();
 #endif
 
 		return builder.Build();
@@ -56,5 +64,54 @@ public class PeripheralsCollection(ILogger<PeripheralsCollection> logger)
 	public void SubscribeOnConnectionEvent(CBCentralManager central)
 	{
 		central.ConnectedPeripheral += (_, e) => Connected = e.Peripheral;
+	}
+}
+
+public static class MauiAppBuilderExtension
+{
+	extension(MauiAppBuilder builder)
+	{
+		public MauiApp BuildWithRegisteringAndValidating<TComponent>() where TComponent : ComponentBase
+		{
+			builder.Services.AddTransient<TComponent>();
+
+			var app = builder.Build();
+
+			var home = app.Services.GetRequiredService<TComponent>();
+		
+			return app;
+		}
+
+		public MauiApp BuildAndValidateBlazor()
+		{
+			var app = builder.Build();
+			
+			app.Services.ResolveAllInjectedInExecutingAssemblyComponents();
+
+			return app;
+		}
+	}
+
+	extension(IServiceProvider provider)
+	{
+		public void ResolveAllInjectedInExecutingAssemblyComponents()
+		{
+			var components = Assembly.GetExecutingAssembly()
+				.GetTypes()
+				.Where(t => t.IsSubclassOf(typeof(ComponentBase)))
+				.ToList();
+
+			var injectedTypes = components
+				.SelectMany(c => c.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+					.Where(p => p.GetCustomAttributes(typeof(InjectAttribute), true).Any())
+					.Select(p => p.PropertyType))
+				.Distinct()
+				.ToList();
+
+			foreach (var type in injectedTypes)
+			{
+				var resolved = provider.GetRequiredService(type);
+			}
+		}
 	}
 }
